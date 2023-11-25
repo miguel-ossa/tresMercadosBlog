3e6729fa84b2c327270292aa85635ec7ab20c443
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2023-11-25 16:34:51 +0000

    Change requirements.txt

--- api/main.py
+++ api/main.py
@@ -1,5 +1,18 @@
 from flask import Flask, render_template
 
+'''
+Make sure the required packages are installed: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from the requirements.txt for this project.
+'''
+
 app = Flask(__name__)
 
 
