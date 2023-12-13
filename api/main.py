077a016f77786f265ffb2768d7bf1c7bb4b2a9dc
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2023-12-13 14:40:56 +0000

    No Disconnect Handling - Pessimistic

--- api/main.py
+++ api/main.py
@@ -63,7 +63,7 @@ gravatar = Gravatar(app,
 
 # CONNECT TO DB
 # sqlite:///posts.db
-engine = create_engine(os.environ.get('DB_URI'), pool_pre_ping=True)
+# engine = create_engine(os.environ.get('DB_URI'), pool_pre_ping=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 db = SQLAlchemy()
 db.init_app(app)
