c25c8df66dfa823866ad590a3b1d04426ad247de
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2024-10-25 09:26:39 +0100

    Refactorización y documentación.

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 import os
 from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash, jsonify
+from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
